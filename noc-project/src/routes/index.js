c1fb054be0d6322d3dc15fc9c378e862bc802268
Luccanjm <lucnjm@gmail.com>
2021-01-27 06:58:32 -0300

    adicionando routes

---
+++ noc-project/src/routes/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {BrowserRouter, Switch, Router} from 'react-router-dom';
+
+const Routes = () => (
+<BrowserRouter>
+<Switch>
+
+</Switch>
+</BrowserRouter>
+);
+export default Routes;
