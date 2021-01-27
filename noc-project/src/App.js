6f3fe9777e4de208a2b0e2baa79dfa9ae553def8
Luccanjm <lucnjm@gmail.com>
2021-01-27 07:10:59 -0300

    alterações

--- noc-project/src/App.js
+++ noc-project/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import Routes from './routes';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import GlobalStyles from './styles/global';
 function App() {
   return (
 <>
 <h1> SISTEMA NOC | SUMICITY</h1>
 <BrowserRouter>
 <Routes />
+
 </BrowserRouter>
+<GlobalStyles />
 </>
   );
 }
